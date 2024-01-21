2b3d22c3b386580ae03459fda8e678067ddd4c35
Y-elv <mugishaelvis456@gmail.com>
2024-01-21 08:31:32 +0200

    adding student in db

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
   <div class="box1">
     <h2>ALL STUDENTS</h2>
     <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">ADD STUDENT</button>
+    
+
   </div>
    <table class="table table-hover table-bordered table-striped">
    <thead>
@@ -46,9 +48,52 @@
     </tbody>
     </table>
 
+
    </div>
    </div>
-    
+
+   <!-- Modal -->
+<form action="insert_data.php" method="post">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">ADD STUDENT</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      
+          <div class="form-group">
+            <label for="f_name">First Name</label>
+            <input type="text"name="f_name"class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="l_name">Last Name</label>
+            <input type="text"name="l_name"class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input type="text"name="age"class="form-control">
+          </div>
+
+       
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="submit" class="btn btn-success"name="add_student"value="ADD">
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 
